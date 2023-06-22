b29c41aed83aec5235f47277bd635e1b1550ca1d
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-22 23:48:01 +0300

    Add . metacharacter

--- validation.js
+++ validation.js
@@ -4,5 +4,6 @@
 * [a-z] Matches any characters between a and z, including a and z.
 * [^a-z] Matches any characters except those in the range a-z.
 * [a-zA-Z] Matches any characters between a-z or A-Z. You can combine as much as you please.
+* . Matches any character other than newline (or including line terminators with the /s flag). Example: (/.+/
 */
 // [abc]: A single character of a...
