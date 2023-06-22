0853410d703409ee52e44077efa87caf0824cc90
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-22 23:45:47 +0300

    Add [^a-z] metacharacter

--- validation.js
+++ validation.js
@@ -2,5 +2,6 @@
 * [abc] A single character of a...
 * [^abc] A character except a..
 * [a-z] Matches any characters between a and z, including a and z.
+* [^a-z] Matches any characters except those in the range a-z.
 */
 // [abc]: A single character of a...
