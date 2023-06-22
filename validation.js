4604858125506eb90a589cc64c1755d64a5c4ba9
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-22 23:46:27 +0300

    Add [a-zA-Z] metacharacter

--- validation.js
+++ validation.js
@@ -3,5 +3,6 @@
 * [^abc] A character except a..
 * [a-z] Matches any characters between a and z, including a and z.
 * [^a-z] Matches any characters except those in the range a-z.
+* [a-zA-Z] Matches any characters between a-z or A-Z. You can combine as much as you please.
 */
 // [abc]: A single character of a...
